b47813eea09b465766845089404c5648cd1a9fd6
Kate Running <118930544+KateRunning@users.noreply.github.com>
2023-04-19 21:27:51 -0500

    updated task app

--- src/App.js
+++ src/App.js
@@ -11,130 +11,80 @@ import Table from './components/Table';
 export default function App() {
   const TASKS_ENDPOINT = 'https://6406aea577c1a905a0e079b5.mockapi.io/V1/tasklist';
 
-  const [task, setTask] = useState([
-    {
-      task: '',
-      date: '',
-    }
-  ])
-  const [tasks, setTasks] = useState(
-    {
-      task: [],
-      date: [],
-    }
-  )
-  const [newTask, setNewTask] = useState({
-    task: '',
-    date: '',
-  })
+  const [task, setTask] = useState(''); //input value hooks
+  const [date, setDate] = useState(''); //input value hooks
+  const [tasks, setTasks] = useState([]) //storing object from 14 & 15
 
-  // const [updatedTask, setUpdatedTask] = useState('')
-
-  function handleTask(x) {
-    setNewTask({
-      ...newTask, //try newTasks
-      task: x,
-    })
+  function handleTask(e) { //updates the state
+    setTask(
+      // ...newTask, //try newTasks
+      e.target.value,
+    )
   }
 
-  const handleChange = (e) => {
-    console.log('handle change')
-    setTask(e.target.value)
+  function handleDate(e) { //updates the state
+    setDate(
+      e.target.value
+    )
   }
 
   const handleEvent = (data) => {
     console.log(data)
-    setTask(data)
-    setNewTask([...newTask, task])
+    const newTask = { date, task, id: 0 } //holds state variables, creates object
+    fetch(TASKS_ENDPOINT, { //enabling API to receive our info
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newTask), //format for data to be received
+    })
+      // setTask(data)
+      .then((response) => response.json())
+      .then((newTask) => setTasks([...tasks, newTask])) //promise returned. spreads tasks array, adds new object to back
+    console.log(tasks)
   }
 
-  const handleDelete = (id) => {
-    console.log('delete')
-    fetch(`${TASKS_ENDPOINT}/${id}`, {
-      method: 'DELETE',
-    }).then(() => getTask())
-  };
 
   useEffect(() => {
     fetch(TASKS_ENDPOINT)
       .then((data) => data.json())
-      .then((data) => handleEvent(data))
+      .then((data) => setTasks(data)) //promise to update state array with new data
   }, [])
 
-  const getTask = () => {  //might be repetitive
-    console.log('get function')
-
-    fetch(TASKS_ENDPOINT)
-      .then((data) => data.json())
-      .then((data) => setTask(data))
-  }
-
-  const postTask = (e) => {
-    // e.preventDefault()
-    console.log('post task')
-
-    fetch(TASKS_ENDPOINT, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newTask),
-    }).then(() => getTask())
-
-    setNewTask({
-      task: '',
-      date: ''
+  const handleDelete = async (id) => {
+    const response = await fetch("/tasks/" + id, {
+      method: "DELETE"
     })
-    
-  }
-  // useEffect(() => {
-  //   const refreshTasks = async () => {
-  //     const response = await fetch(TASKS_ENDPOINT)
-  //     const data = await response.json()
-  //     setTask(data)
-  //   }
-  //   refreshTasks()
-  // }, [])
-
-  const onDelete = async (id) => {
-    await deleteTask(id)
-    setTask(task.filter(t => t.id !== id))
-  }
-
-  const createTask = async (newTask) => {
-    const response = await fetch(TASKS_ENDPOINT, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newTask)
-    })
-    const createdTask = await response.json()
-    setTask(task.concat(createdTask))
-    createTask()
-  }
-
-  const updateTask = async (updatedTask) => {
-    fetch(TASKS_ENDPOINT + updatedTask.id, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(updatedTask)
-
-    })
-    // setTask(task.map(t => t.id === updatedTask.id ? updatedTask : t))
+    console.log("delete")
+    setTasks(tasks.filter(t => t.id !== id))
   }
 
   return (
+
     // <ul>
     //   {task.map(t => <li>{t.name} <button onClick={() => onDelete(t.id)}>Delete</button></li>)}
     // </ul>
     <div className="App">
-      <Form
-        handleTask={handleTask}
+      <input type='text' onChange={handleTask}>
+
+      </input>
+      <input type='date' onChange={handleDate}></input>
+      <button onClick={handleEvent}>Submit</button>
+
+      {tasks.map((t, i) => <li key={i}>{t.task}{t.date}
+        <button onClick={handleDelete}>Delete</button></li>)}
+
+   
+
+      {/* <Form
+
+        
         // onDelete={onDelete}
         handleEvent={handleEvent}
-        createTask={createTask}
-        task={task}
-      />
+        // createTask={createTask}
+        // task={task}
+      /> */}
       <Table
-      handleChange={handleChange}
-      task={task}
+      // handleChange={handleChange}
+      // task={task}
       // updateTask={updateTask}
       // handleDelete={handleDelete}
       />
--- src/components/onDelete.js
+++ src/components/onDelete.js
@@ -1,3 +1,12 @@
+import React from 'react'
+
+export default function onDelete() {
+  return (
+    <div></div>
+  )
+}
+
+
 export const deleteTask = async (id) => {
     const response = await fetch("/tasks/" + id, {
         method: 'DELETE'
